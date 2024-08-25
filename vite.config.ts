1b23be30dcb3ad9377a449028937a684c9f24eb1
mesfin-2 <mesfintdev@gmail.com>
2024-08-25 09:29:27 +0300

    build app

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.ts
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // Ensure this matches where you want the output
+  },
+});
